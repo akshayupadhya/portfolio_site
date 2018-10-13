a0711cd4d1c675992c31ce598f8371ee63823d2b
akshay upadhya <akshaysrinagar@gmail.com>
2018-10-13 22:08:23 +0530

    layout of the index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,1 +1,20 @@
-export default () => <div>Welcome to next.js!</div>
+import { Component } from 'react'
+import OneScroll from '../components/OneScroll'
+require('../styles.css')
+export default class Home extends Component {
+  render () {
+    return (
+      <div>
+        <OneScroll className="first">
+          name ,intro,resume,gitHub and twitter Links
+        </OneScroll> 
+        <OneScroll>
+          projects
+        </OneScroll> 
+        <OneScroll>
+          About this project?maybe.... why not(talking about CSS grid,github api for projects to diplay stars and description,talking about projects route)
+        </OneScroll> 
+      </div>
+    )
+  }
+}
