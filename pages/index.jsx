4473c158c136fbc977ae6507d2615b465cac23e9
akshay upadhya <akshaysrinagar@gmail.com>
2018-10-21 23:23:18 +0530

    Revert "profile section JSON"

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,24 @@
 import { Component } from 'react'
+import { OneScroll } from '../components/OneScroll'
 import {FirstScrollHome} from '../components/firstScrollHome'
 import {SecondScrollHome} from '../components/secondScrollHome'
-import {ThirdScrollHome} from '../components/thidScrollHome'
 import '../scss/style.scss'
 import '../scss/pages/index.scss'
 export default class Home extends Component {
   render () {
     return (
       <div className="home">
+        {/* <OneScroll className="first">
+          name ,intro,resume,gitHub and twitter Links
+        </OneScroll>  */}
+        {/* <OneScroll>
+          projects
+        </OneScroll>  */}
         <FirstScrollHome/>
         <SecondScrollHome/>
-        <ThirdScrollHome/> 
+        <OneScroll>
+          About this project?maybe.... why not(talking about CSS grid,github api for projects to diplay stars and description,talking about projects route)
+        </OneScroll> 
       </div>
     )
   }
