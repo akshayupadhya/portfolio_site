d4688e788c9324ba1512ff5e6dee5427289e2849
akshay upadhya <akshaysrinagar@gmail.com>
2019-02-02 23:04:31 +0530

    cleaning up firstScrollHome

--- components/firstScrollHome.jsx
+++ components/firstScrollHome.jsx
@@ -3,59 +3,54 @@ import "../scss/components/firstScrollHome.scss";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
 import {
-	faGithub,
-	faGithubSquare,
-	faTwitter,
-	faTwitterSquare,
-	faLinkedinIn,
-	faLinkedin
+  faGithub,
+  faGithubSquare,
+  faTwitter,
+  faTwitterSquare,
+  faLinkedinIn,
+  faLinkedin
 } from "@fortawesome/fontawesome-free-brands";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconBtn } from "../components/iconBtn";
 library.add(
-	faEnvelope,
-	faKey,
-	faTwitter,
-	faGithub,
-	faLinkedinIn,
-	faTwitterSquare,
-	faLinkedin,
-	faGithubSquare
+  faEnvelope,
+  faKey,
+  faTwitter,
+  faGithub,
+  faLinkedinIn,
+  faTwitterSquare,
+  faLinkedin,
+  faGithubSquare
 );
+const socialIcons = [
+  {
+    icon: faGithub,
+    url: "https://github.com/akshayupadhya"
+  },
+  {
+    icon: faTwitterSquare,
+    url: "https://twitter.com/kcification"
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com/in/akshay-upadhya-68548156/"
+  }
+];
+const socialIconColor = "#fff";
 export const FirstScrollHome = () => (
-	<OneScroll className="firstScroll">
-		<div className="intro">
-			<div className="circle" />
-			<p className="name">Akshay Upadhya</p>
-			<div className="job">
-				<p className="designation">FrontEnd developer ,</p>
-				<p className="company">Jigsaw Academy</p>
-			</div>
-			<p className="place">Bangalore</p>
-		</div>
-		<div className="social-media">
-			<IconBtn
-				icon={faGithubSquare}
-				color="#6DB65B"
-				size="5"
-				url="https://github.com/akshayupadhya"
-				target="blank"
-			/>
-			<IconBtn
-				icon={faTwitterSquare}
-				className="twitter"
-				color="#6DB65B"
-				size="5"
-				url="https://twitter.com/kcification"
-				target="blank"
-			/>
-			<IconBtn
-				icon={faLinkedin}
-				color="#6DB65B"
-				size="5"
-				url="https://www.linkedin.com/in/akshay-upadhya-68548156/"
-				target="blank"
-			/>
-		</div>
-	</OneScroll>
+  <OneScroll className="firstScroll">
+    <div className="intro">
+      <div className="circle" />
+      <p className="name">Akshay Upadhya</p>
+      <div className="job">
+        <p className="designation">FrontEnd developer ,</p>
+        <p className="company">Jigsaw Academy</p>
+      </div>
+      <p className="place">Bangalore</p>
+    </div>
+    <div className="social-media">
+      {socialIcons.map(({ icon, url }) => (
+        <IconBtn icon={icon} url={url} color={`${socialIconColor}`} />
+      ))}
+    </div>
+  </OneScroll>
 );
--- components/iconBtn.jsx
+++ components/iconBtn.jsx
@@ -1,11 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export const IconBtn = (prop)=>(
-  <a href={prop.url} target={prop.target}>
-    <FontAwesomeIcon
-      icon={prop.icon}
-      color={prop.color}
-      size={`${prop.size}x`}
-      className={prop.className}
-    />
-  </a>
-)
+export const IconBtn = ({ url, target, icon, color, size, className }) => {
+  console.info(url, size, className);
+  return (
+    <a href={url} target="_blank">
+      <FontAwesomeIcon
+        icon={icon}
+        color={color}
+        size="5x"
+        className={className}
+      />
+    </a>
+  );
+};
