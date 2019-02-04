c2203bba84674461dd6ee81d2ccec392d572023f
akshay upadhya <akshaysrinagar@gmail.com>
2019-02-05 00:26:33 +0530

    logic fixes and timeout in didmount

--- components/routeHandler.jsx
+++ components/routeHandler.jsx
@@ -1,29 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Transition, config } from "react-spring";
 const routes = ["/", "/projects", "/bio"];
 export const RouteHandler = ({ currentRoute, children, url }) => {
   const [tapStart, RecordTap] = useState(undefined);
   const currentRouteIndex = routes.indexOf(currentRoute);
   const changeRoute = type => {
     let index = currentRouteIndex;
-    switch (type) {
-      case "up":
-        index += 1;
-        break;
-      case "down":
-        index -= 1;
-        break;
-    }
+    if (type == "up") index -= 1;
+    else index += 1;
     if (index >= 0 && index <= routes.length - 1) {
       url.push(routes[index]);
     }
   };
-  const ScrollHandler = (type, e) => {
+  let timer = false;
+  useEffect(() => {
+    // i know this is a freaking hack, will definely find a better way to this, but thats it for now
+    setTimeout(() => {
+      timer = true;
+    }, 500);
+  }, []);
+  const ScrollHandler = (type, e, element) => {
     e.preventDefault();
     switch (type) {
       case "tapStart": {
         //coz i like brackets
         RecordTap(e.touches[0].clientY);
-        console.log(e.touches[0].clientY);
         break;
       }
       case "tapEnd": {
@@ -35,9 +36,16 @@ export const RouteHandler = ({ currentRoute, children, url }) => {
         break;
       }
       case "mouse": {
-        if (tapStart === undefined) {
-          if (e.deltaY > 0) changeRoute("up");
-          else changeRoute("down");
+        if (tapStart === undefined && timer) {
+          if (e.nativeEvent.deltaY <= 0) {
+            /* scrolling up */
+            e.preventDefault();
+            changeRoute("up");
+          } else {
+            /* scrolling down */
+            e.preventDefault();
+            changeRoute("down");
+          }
         }
         break;
       }
@@ -45,11 +53,19 @@ export const RouteHandler = ({ currentRoute, children, url }) => {
   };
   return (
     <div
-      className="routeHandler"
+      className="route-handler"
       onTouchStart={e => ScrollHandler("tapStart", e)}
       onTouchEnd={e => ScrollHandler("tapEnd", e)}
-      onWheel={e => ScrollHandler("mouse", e)}>
-      {children}
+      onWheel={e => ScrollHandler("mouse", e, this)}>
+      <Transition
+        config={config.slow}
+        from={{ opacity: 0 }}
+        enter={{ opacity: 1 }}
+        leave={{ opacity: 0 }}>
+        {style => props => (
+          <div style={{ ...props, style, height: `100%` }}>{children}</div>
+        )}
+      </Transition>
     </div>
   );
 };
