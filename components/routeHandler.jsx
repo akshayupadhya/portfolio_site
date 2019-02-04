a3768bae2140eda9b167cafc2c784a8ad929b68c
akshay upadhya <akshaysrinagar@gmail.com>
2019-02-04 09:58:49 +0530

    simpler changeRoute

--- components/routeHandler.jsx
+++ components/routeHandler.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 const routes = ["/", "/projects", "/bio"];
-
 export const RouteHandler = ({ currentRoute, children, url }) => {
   const [tapStart, RecordTap] = useState(undefined);
   const currentRouteIndex = routes.indexOf(currentRoute);
-  const changeRoute = index => {
+  const changeRoute = type => {
+    let index = currentRouteIndex;
+    switch (type) {
+      case "up":
+        index += 1;
+        break;
+      case "down":
+        index -= 1;
+        break;
+    }
     if (index >= 0 && index <= routes.length - 1) {
       url.push(routes[index]);
     }
@@ -21,14 +29,14 @@ export const RouteHandler = ({ currentRoute, children, url }) => {
       case "tapEnd": {
         if (tapStart > e.changedTouches[0].clientY)
           // scroll up
-          changeRoute(currentRouteIndex + 1);
+          changeRoute("up");
         // scroll down
-        changeRoute(currentRouteIndex - 1);
+        else changeRoute("down");
       }
       case "mouse": {
         if (tapStart === undefined) {
-          if (e.deltaY > 0) changeRoute(currentRouteIndex + 1);
-          else changeRoute(currentRouteIndex - 1);
+          if (e.deltaY > 0) changeRoute("up");
+          else changeRoute("down");
         }
         break;
       }
@@ -39,8 +47,7 @@ export const RouteHandler = ({ currentRoute, children, url }) => {
       className="routeHandler"
       onTouchStart={e => ScrollHandler("tapStart", e)}
       onTouchEnd={e => ScrollHandler("tapEnd", e)}
-      onWheel={e => ScrollHandler("mouse", e)}
-    >
+      onWheel={e => ScrollHandler("mouse", e)}>
       {children}
     </div>
   );
