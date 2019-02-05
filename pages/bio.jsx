bc607270300a401496670f01c08d5fe6ebec5c5d
akshay upadhya <akshaysrinagar@gmail.com>
2019-02-06 01:22:43 +0530

    renaming components

--- pages/bio.jsx
+++ pages/bio.jsx
@@ -1,9 +1,32 @@
-import { ThirdScrollHome } from "../components/thidScrollHome";
-import { RouteHandler } from "../components/routeHandler";
-export default function Bio({ url }) {
+import "../scss/components/thirdScrollHome.scss";
+import me from "../data/bio.json";
+const TabChar = (match, offset, string) => (offset > 0 ? `\n->->` : "");
+export const Bio = () => {
   return (
-    <RouteHandler url={url} currentRoute="/bio">
-      <ThirdScrollHome />
-    </RouteHandler>
+    <div className="bio">
+      <div className="title">About me</div>
+      <code>
+        <pre>
+          {// JSON.stringify(me, null, 1)
+          JSON.stringify(
+            me,
+            function(key, value) {
+              if (value instanceof Array) return JSON.stringify(value);
+              return value;
+            },
+            2
+          )
+            .replace(/"\[/g, "[")
+            .replace(/\]"/g, "]")
+            .replace(/\\"/g, '"')
+            .replace(/""/g, '"')
+            // .replace(/\n/g,TabChar)// will fix it some day
+            // .replace(/->\s{2}/g)
+            .replace(/  /g, "->|")
+            .replace(/ /g, "·")}
+        </pre>
+      </code>
+    </div>
   );
-}
+};
+export default Bio;
