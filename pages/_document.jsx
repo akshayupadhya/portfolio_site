fabe234e56d37c10d67a6f096f2ba3732f427e11
akshay upadhya <akshaysrinagar@gmail.com>
2019-02-06 01:20:49 +0530

    clean _document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,36 +1,33 @@
-import React, { createContext, useState } from "react";
 import Document, { Head, Main, NextScript } from "next/document";
 import Manifest from "next-manifest/manifest";
 import { OneScroll } from "../components/OneScroll";
-import gitdata from "../data/data.json";
-export const gitContext = new createContext();
-export default function MyDocument() {
-  const [data, updateGitdata] = useState(gitdata);
-  return (
-    <html>
-      <Head>
-        <title>Akshay Upadhya</title>
-        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Manifest
-          href="/static/manifest/manifest.json"
-          // color for `theme-color`
-          themeColor="#F0F0F0"
-          // scale for `viewport` meta tag
-          initialScale="1"
-          display="fullScreen"
-        />
-        <link rel="stylesheet" href="/_next/static/style.css" />
-      </Head>
-      <body>
-        <OneScroll>
-          <gitContext.provider value={{ gitRepos: data }}>
-            <Main />
-          </gitContext.provider>
-        </OneScroll>
-        <NextScript />
-      </body>
-    </html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Akshay Upadhya</title>
+          <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <Manifest
+            href="/static/manifest/manifest.json"
+            // color for `theme-color`
+            themeColor="#F0F0F0"
+            // scale for `viewport` meta tag
+            initialScale="1"
+            display="fullScreen"
+          />
+          <link rel="stylesheet" href="/_next/static/style.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
 }
