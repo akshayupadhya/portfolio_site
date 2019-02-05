3e6b8b15eff7fdec82821252ed52d64508bded42
akshay upadhya <akshaysrinagar@gmail.com>
2019-02-06 01:21:16 +0530

    adding context for github data

--- components/OneScroll.jsx
+++ components/OneScroll.jsx
@@ -1,9 +1,32 @@
+import { createContext, useState, useEffect } from "react";
 import "../scss/components/oneScroll.scss";
-export const OneScroll = props => {
-  const className = props.className;
+import gitData from "../data/data.json";
+export const GitContext = createContext({
+  data: gitData
+});
+export const OneScroll = ({ className, children }) => {
+  const [data, addGitData] = useState(gitData);
+  useEffect(async () => {
+    console.info(`oneScroll`);
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/akshayupadhya/repos`,
+        {
+          headers: {
+            Accept: "application/json"
+          }
+        }
+      );
+      const gData = await response.json();
+      // console.table(gData);
+      addGitData(gData);
+    } catch (e) {
+      console.error(`failed to fetch`, e);
+    }
+  }, []);
   return (
     <div className={`OneScroll ${className ? className : ""}`}>
-      {props.children}
+      <GitContext.Provider value={{ data }}>{children}</GitContext.Provider>
     </div>
   );
 };
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,9 +1,44 @@
-import { SecondScrollHome } from "../components/secondScrollHome";
-import { RouteHandler } from "../components/routeHandler";
-export default function({ url }) {
+import { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGripHorizontal,
+  faGripVertical
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../scss/components/secondScrollHome.scss";
+import { GitContext } from "../components/OneScroll";
+library.add(faGripHorizontal, faGripVertical);
+export const Projects = () => {
+  const [counter, updateCounter] = useState(0);
+  const classNames = ["oneThird", "oneFourth", "half", "fullWidth"];
+
+  const classToggle = () => updateCounter((counter + 1) % classNames.length);
+
   return (
-    <RouteHandler url={url} currentRoute="/projects">
-      <SecondScrollHome />
-    </RouteHandler>
+    <div className="projects">
+      <div className="title">
+        <div>Projects</div>
+        <FontAwesomeIcon
+          icon={counter % 2 === 0 ? faGripHorizontal : faGripVertical}
+          onClick={classToggle}
+          size="2x"
+        />
+      </div>
+      <div className={`projects ${classNames[counter]}`} />
+      <GitContext.Consumer>
+        {context => (
+          <div>
+            {[...(context.data || null)]
+              .filter(({ stargazers_count }) => stargazers_count)
+              .map(({ name, stargazers_count }, j) => (
+                <p key={j}>
+                  {name},{stargazers_count}
+                </p>
+              ))}
+          </div>
+        )}
+      </GitContext.Consumer>
+    </div>
   );
-}
+};
+export default Projects;
